20d1013eb8bf4b6ededff582fd1926e589014f97
Caleb Borwick <50631296+CalebBorwick@users.noreply.github.com>
2020-06-23 14:38:24 -0700

    Delete InexistentKeyException.java

--- Assignment 4/Ian Borwick/InexistentKeyException.java
+++
@@ -1,7 +0,0 @@
-
-//Exception class for if a key is not existing 
-public class InexistentKeyException extends RuntimeException {
-    public InexistentKeyException() {
-        super("Key not in tree");
-    }
-}
