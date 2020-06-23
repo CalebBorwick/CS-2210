5204c3bafd9a264b8bdf526889c57972e6a0cf52
Caleb Borwick <50631296+CalebBorwick@users.noreply.github.com>
2020-06-23 14:38:07 -0700

    Delete DuplicatedKeyException.java

--- Assignment 4/Ian Borwick/DuplicatedKeyException.java
+++
@@ -1,7 +0,0 @@
-//exception class for if a key is a duplicate
-public class DuplicatedKeyException extends RuntimeException {
-    public DuplicatedKeyException() {
-        super("Cannot insert key because it already exists in tree");
-    }
- 
-}
